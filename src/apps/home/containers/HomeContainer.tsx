6d01c0252ca3ed1226a039807920c17b292d61eb
lzkmylz <lzkmylz@163.com>
2019-03-23 14:51:21 +0800

    backup

--- src/apps/home/containers/HomeContainer.tsx
+++ src/apps/home/containers/HomeContainer.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
 import HomeStore from '../stores/HomeStore';
 
+import Button from 'antd/lib/button';
+import '../styles/HomeContainer.css';
+
 class HomeContainer extends React.Component {
+  handleClick = (): void => {
+    HomeStore.changeUsername();
+  }
+
   public render(): React.ReactNode {
     return (
       <div>
-        This is HomeContainer!
-        The username is: { HomeStore.username }
+        <p>
+          This is HomeContainer!
+          The username is: { HomeStore.username }
+        </p>
+        <Button
+          type="primary"
+          onClick={this.handleClick}
+        >
+          change username
+        </Button>
       </div>
     )
   }
